1f9cd9166ac7c79d2458ca3de4975dedead2a4a4
Federico Gobbo <federico.gobbo.2@studenti.unipd.it>
2017-04-02 16:07:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  # word.start_with? /[aeiou]/
+  # !!word.match(/\A[aeiou]/)
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.split.select { |el| el.match(/^un\w+ing$/) }
 end
 
 def words_five_letters_long(text)
-
+  text.split.select { |el| el.match(/^\w{5}$/)}
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[.,?!:;]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\(?\d{3}[\)\s]?\d{3}[-\s]?\d{4}/)
 end
